4bd44cc1f7f598940cde624d05049fdc5ba5f0e2
Tohir <TOXIR_DEV>
2022-10-12 11:28:56 +0600

    changed breakpoint

--- feature/modal-slice/index.js
+++ feature/modal-slice/index.js
@@ -3,24 +3,24 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  isOpenModal: false,
-  isOpenSuccessModal: false,
+    isOpenModal: false,
+    isOpenSuccessModal: true,
 };
 
 const modalSlice = createSlice({
-  name: "modal",
-  initialState,
-  reducers: {
-    closeModal(state) {
-      state.isOpenModal = false;
+    name: "modal",
+    initialState,
+    reducers: {
+        closeModal(state) {
+            state.isOpenModal = false;
+        },
+        openModal(state) {
+            state.isOpenModal = true;
+        },
+        openSuccessModal(state) {
+            state.isOpenSuccessModal = true;
+        },
     },
-    openModal(state) {
-      state.isOpenModal = true;
-    },
-    openSuccessModal(state) {
-      state.isOpenSuccessModal = true;
-    },
-  },
 });
 
 export const { closeModal, openModal, openSuccessModal } = modalSlice.actions;
